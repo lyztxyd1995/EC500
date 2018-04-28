856a6f8a67e413291cb4a900b4809b5ca3120958
Yize Liu <31735325+lyztxyd1995@users.noreply.github.com>
2018-04-27 22:45:40 -0400

    Add files via upload

---
+++ HW3_MongoDB/phase_3.py
@@ -0,0 +1,28 @@
+import MongoDBPython
+
+from MongoDBPython import DatabaseUpdate
+
+DatabaseUpdate('katyperry');
+DatabaseUpdate('justinbieber');
+DatabaseUpdate('BarackObama');
+DatabaseUpdate('rihanna');
+DatabaseUpdate('taylorswift13');
+DatabaseUpdate('ladygaga');
+DatabaseUpdate('TheEllenShow');
+DatabaseUpdate('Cristiano');
+DatabaseUpdate('Youtube');
+DatabaseUpdate('jtimberlake');
+DatabaseUpdate('twitter');
+DatabaseUpdate('KimKardashian');
+DatabaseUpdate('britneyspears');
+DatabaseUpdate('ArianaGrande');
+DatabaseUpdate('selenagomez');
+DatabaseUpdate('ddlovato');
+DatabaseUpdate('cnnbrk');
+DatabaseUpdate('shakira');
+DatabaseUpdate('jimmyfallon');
+DatabaseUpdate('realDonaldTrump');
+print('done 20');
+
+from MongoDBPython import TotalTags
+data = TotalTags();
